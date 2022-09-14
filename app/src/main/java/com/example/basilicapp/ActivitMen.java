e8889bb0d44018c0a2f38842c39031739f4a21fa
Pavel Lukyanovich <paellukyanovich1@gmail.com>
2022-09-14 12:50:01 +0300

    UI first activyty

---
+++ app/src/main/java/com/example/basilicapp/ActivitMen.java
@@ -0,0 +1,14 @@
+package com.example.basilicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivitMen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activit_men);
+    }
+}
--- app/src/main/java/com/example/basilicapp/MainActivity.java
+++ app/src/main/java/com/example/basilicapp/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.basilicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageViewChooseIndicator;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageViewChooseIndicator = findViewById(R.id.chooseImageView);
+            imageViewChooseIndicator.setScaleY(0.6f);
+            imageViewChooseIndicator.setScaleX(0.6f);
+            imageViewChooseIndicator.animate().scaleY(1.0f).scaleX(1.0f).setDuration(2000);
+        }
+
+    public void runMenActivity(View view) {
+        Intent intent =new Intent(this, ActivityMen);
     }
 }
